00d0a34515b37b9ee9d51a356729aca04c02741b
dandirivaldimointi404 <hilenkelo0505@gmail.com>
2023-12-12 22:12:14 +0800

    Splash Login

--- app/src/main/java/com/example/my_smk_app/Activity/SplashScreen.java
+++ app/src/main/java/com/example/my_smk_app/Activity/SplashScreen.java
@@ -2,15 +2,43 @@ package com.example.my_smk_app.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
 
+import com.airbnb.lottie.LottieAnimationView;
 import com.example.my_smk_app.R;
 
 public class SplashScreen extends AppCompatActivity {
 
+    private static final String TAG = SplashScreen.class.getSimpleName();
+    private LottieAnimationView lottie;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_splash_screen);
+
+        lottie = findViewById(R.id.lottie);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Intent i = new Intent(getApplicationContext(), LoginActivity.class);
+                    startActivity(i);
+                    finish();
+                } catch (Exception e) {
+                    Log.e(TAG, "Error starting LoginActivity", e);
+                }
+            }
+        }, 5000);
     }
 }
